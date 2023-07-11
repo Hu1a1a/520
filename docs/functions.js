e9b27158607eeb4b49a2053c1e7025e8d4236cf7
Hulala <yang.ye.1@hotmail.com>
2023-07-11 23:12:01 +0200

    11/07/23

--- docs/functions.js
+++ docs/functions.js
@@ -41,7 +41,7 @@ $(window).resize(function () {
 
 function timeElapse(date) {
   var current = Date();
-  var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
+  var seconds = (Date.parse(current) - Date.parse(date)) / 1000 + 30 * 24 * 3600;
   var days = Math.floor(seconds / (3600 * 24));
   seconds = seconds % (3600 * 24);
   var hours = Math.floor(seconds / 3600);
